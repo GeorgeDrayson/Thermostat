350df5ad4914eb23bba07368b12d8ae0360d02a7
George Drayson <drayson.g@mac.com>
2018-04-23 12:17:56 +0100

    Added time function and changed HTML

--- jasmine/interface.js
+++ jasmine/interface.js
@@ -1,7 +1,36 @@
 $( document ).ready(function() {
 
-  var thermostat = new Thermostat();
-  updateTemp()
+  setInterval(function() {
+    var date = new Date();
+    $('#clock').html(
+        date.getHours() + ":" + checkTime(date.getMinutes()) + ":" + checkTime(date.getSeconds())
+        );
+}, 500);
+
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+var thermostat = new Thermostat();
+updateTemp()
+
+$('#select-city').submit( function(event) {
+  event.preventDefault();
+  var city = $('#location').val();
+  displayWeather(city);
+})
+
+
+function displayWeather(city) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+  var metric = '&units=metric';
+  $.get((url + token + metric), function(data) {
+    $('#outside-temp').text(city + ": " + data.main.temp + "°C");
+  });
+}
 
 $('#temp_up').click(function() {
    thermostat.up(1);
@@ -25,7 +54,7 @@ $('#temp_up').click(function() {
     });
 
   function updateTemp() {
-    $('#temperature').text(thermostat._temperature);
+    $('#temperature').text(thermostat._temperature + "°C");
     $("#dot").attr('class', thermostat.usage());
   }
 
